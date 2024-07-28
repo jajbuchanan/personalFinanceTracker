13d9af8b627f9714edd811eadee14067b82fc317
ThePrimeagen <the.primeagen@aol.com>
2024-07-28 11:01:12 +0100

    add a data masking feature

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from data_entry import get_amount, get_category, get_date, get_description
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initalize_csv(cls):
@@ -32,6 +33,40 @@ class CSV:
 
     print("Entry added successfully")
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.FORMAT)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transactions found in the given date range.")
+        else:
+            print(
+                f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}"
+            )
+            print(
+                filtered_df.to_string(
+                    index=False, formatters={"date": lambda x: x.strftime(CSV.FORMAT)}
+                )
+            )
+
+            total_income = filtered_df[filtered_df["category"] == "Income"][
+                "amount"
+            ].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"][
+                "amount"
+            ].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2f}")
+
+        return
+
 
 def add():
     CSV.initalize_csv()
@@ -45,4 +80,4 @@ def add():
     CSV.add_entry(date, amount, category, description)
 
 
-add()
+CSV.get_transactions("01-01-2023", "30-07-2024")
